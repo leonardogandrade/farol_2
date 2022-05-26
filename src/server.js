60e03f9720c898f9a5a5afa97ff27a243b387682
leonardogandrade <leonardo.g.andrade@gmail.com>
2022-05-26 18:06:58 -0300

    heroku

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+const cors = require('cors');
 const server = (db) => {
     const app = express();
+    app.use(cors());
     app.use(express.json())
 
     require('./routes')(app, db)
