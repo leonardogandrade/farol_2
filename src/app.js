f5c7e66c7b9cc8ec557aed69fa5dc58f6a5daafa
leonardogandrade <leonardo.g.andrade@gmail.com>
2022-05-26 18:20:26 -0300

    heroku

--- src/app.js
+++ src/app.js
@@ -2,11 +2,10 @@ const server = require('./server');
 const repository = require('../src/models/devotional-model');
 const { dbConnect } = require('../src/db/connection');
 const app = server(repository);
-const PORT = process.env.PORT;
 
 dbConnect();
 
-app.listen(PORT | 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`[devotional] UP:${PORT}`)
 }).on('error_devotional', (err) => {
     console.log(`ERROR: ${err}`);
